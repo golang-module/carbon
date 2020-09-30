0d5d21eb488f3126d0588c29a2f6c32822cb7f0f
gouguoyin <245629560@qq.com>
2020-10-01 00:22:11 +0800

    完善单元测试

--- carbon_test.go
+++ carbon_test.go
@@ -96,6 +96,267 @@ func TestCarbon_CreateFromTime(t *testing.T) {
 	}
 }
 
+func TestCarbon_CreateFromString(t *testing.T) {
+	now := time.Now().Add(time.Hour * 2)
+	e := now.Format("2006-01-02 15:04:05")
+	r := New().Now().CreateFromString("2h").ToDateTimeString()
+	if r != e {
+		t.Fatalf("Expected %s, but got %s", e, r)
+	}
+}
+
+func TestCarbon_AddYears(t *testing.T) {
+	now := time.Now().AddDate(2, 0, 0)
+	e := now.Format("2006-01-02 15:04:05")
+	r := New().Now().AddYears(2).ToDateTimeString()
+	if r != e {
+		t.Fatalf("Expected %s, but got %s", e, r)
+	}
+}
+
+func TestCarbon_SubYears(t *testing.T) {
+	now := time.Now().AddDate(-2, 0, 0)
+	e := now.Format("2006-01-02 15:04:05")
+	r := New().Now().SubYears(2).ToDateTimeString()
+	if r != e {
+		t.Fatalf("Expected %s, but got %s", e, r)
+	}
+}
+
+func TestCarbon_AddYear(t *testing.T) {
+	now := time.Now().AddDate(1, 0, 0)
+	e := now.Format("2006-01-02 15:04:05")
+	r := New().Now().AddYear().ToDateTimeString()
+	if r != e {
+		t.Fatalf("Expected %s, but got %s", e, r)
+	}
+}
+
+func TestCarbon_SubYear(t *testing.T) {
+	now := time.Now().AddDate(-1, 0, 0)
+	e := now.Format("2006-01-02 15:04:05")
+	r := New().Now().SubYear().ToDateTimeString()
+	if r != e {
+		t.Fatalf("Expected %s, but got %s", e, r)
+	}
+}
+
+func TestCarbon_AddMonths(t *testing.T) {
+	now := time.Now().AddDate(0, 2, 0)
+	e := now.Format("2006-01-02 15:04:05")
+	r := New().Now().AddMonths(2).ToDateTimeString()
+	if r != e {
+		t.Fatalf("Expected %s, but got %s", e, r)
+	}
+}
+
+func TestCarbon_SubMonths(t *testing.T) {
+	now := time.Now().AddDate(0, -2, 0)
+	e := now.Format("2006-01-02 15:04:05")
+	r := New().Now().SubMonths(2).ToDateTimeString()
+	if r != e {
+		t.Fatalf("Expected %s, but got %s", e, r)
+	}
+}
+
+func TestCarbon_AddMonth(t *testing.T) {
+	now := time.Now().AddDate(0, 1, 0)
+	e := now.Format("2006-01-02 15:04:05")
+	r := New().Now().AddMonth().ToDateTimeString()
+	if r != e {
+		t.Fatalf("Expected %s, but got %s", e, r)
+	}
+}
+
+func TestCarbon_SubMonth(t *testing.T) {
+	now := time.Now().AddDate(0, -1, 0)
+	e := now.Format("2006-01-02 15:04:05")
+	r := New().Now().SubMonth().ToDateTimeString()
+	if r != e {
+		t.Fatalf("Expected %s, but got %s", e, r)
+	}
+}
+
+func TestCarbon_AddWeeks(t *testing.T) {
+	now := time.Now().AddDate(0, 0, 14)
+	e := now.Format("2006-01-02 15:04:05")
+	r := New().Now().AddWeeks(2).ToDateTimeString()
+	if r != e {
+		t.Fatalf("Expected %s, but got %s", e, r)
+	}
+}
+
+func TestCarbon_SubWeeks(t *testing.T) {
+	now := time.Now().AddDate(0, 0, -14)
+	e := now.Format("2006-01-02 15:04:05")
+	r := New().Now().SubWeeks(2).ToDateTimeString()
+	if r != e {
+		t.Fatalf("Expected %s, but got %s", e, r)
+	}
+}
+
+func TestCarbon_AddWeek(t *testing.T) {
+	now := time.Now().AddDate(0, 0, 7)
+	e := now.Format("2006-01-02 15:04:05")
+	r := New().Now().AddWeek().ToDateTimeString()
+	if r != e {
+		t.Fatalf("Expected %s, but got %s", e, r)
+	}
+}
+
+func TestCarbon_SubWeek(t *testing.T) {
+	now := time.Now().AddDate(0, 0, -7)
+	e := now.Format("2006-01-02 15:04:05")
+	r := New().Now().SubWeek().ToDateTimeString()
+	if r != e {
+		t.Fatalf("Expected %s, but got %s", e, r)
+	}
+}
+
+func TestCarbon_AddDays(t *testing.T) {
+	now := time.Now().AddDate(0, 0, 2)
+	e := now.Format("2006-01-02 15:04:05")
+	r := New().Now().AddDays(2).ToDateTimeString()
+	if r != e {
+		t.Fatalf("Expected %s, but got %s", e, r)
+	}
+}
+
+func TestCarbon_SubDays(t *testing.T) {
+	now := time.Now().AddDate(0, 0, -2)
+	e := now.Format("2006-01-02 15:04:05")
+	r := New().Now().SubDays(2).ToDateTimeString()
+	if r != e {
+		t.Fatalf("Expected %s, but got %s", e, r)
+	}
+}
+
+func TestCarbon_AddDay(t *testing.T) {
+	now := time.Now().AddDate(0, 0, 1)
+	e := now.Format("2006-01-02 15:04:05")
+	r := New().Now().AddDay().ToDateTimeString()
+	if r != e {
+		t.Fatalf("Expected %s, but got %s", e, r)
+	}
+}
+
+func TestCarbon_SubDay(t *testing.T) {
+	now := time.Now().AddDate(0, 0, -1)
+	e := now.Format("2006-01-02 15:04:05")
+	r := New().Now().SubDay().ToDateTimeString()
+	if r != e {
+		t.Fatalf("Expected %s, but got %s", e, r)
+	}
+}
+
+func TestCarbon_AddHours(t *testing.T) {
+	now := time.Now().Add(time.Hour * 2)
+	e := now.Format("2006-01-02 15:04:05")
+	r := New().Now().AddHours(2).ToDateTimeString()
+	if r != e {
+		t.Fatalf("Expected %s, but got %s", e, r)
+	}
+}
+
+func TestCarbon_SubHours(t *testing.T) {
+	now := time.Now().Add(time.Hour * -2)
+	e := now.Format("2006-01-02 15:04:05")
+	r := New().Now().SubHours(2).ToDateTimeString()
+	if r != e {
+		t.Fatalf("Expected %s, but got %s", e, r)
+	}
+}
+
+func TestCarbon_AddHour(t *testing.T) {
+	now := time.Now().Add(time.Hour)
+	e := now.Format("2006-01-02 15:04:05")
+	r := New().Now().AddHour().ToDateTimeString()
+	if r != e {
+		t.Fatalf("Expected %s, but got %s", e, r)
+	}
+}
+
+func TestCarbon_SubHour(t *testing.T) {
+	now := time.Now().Add(-time.Hour)
+	e := now.Format("2006-01-02 15:04:05")
+	r := New().Now().SubHour().ToDateTimeString()
+	if r != e {
+		t.Fatalf("Expected %s, but got %s", e, r)
+	}
+}
+
+func TestCarbon_AddMinutes(t *testing.T) {
+	now := time.Now().Add(time.Minute * 2)
+	e := now.Format("2006-01-02 15:04:05")
+	r := New().Now().AddMinutes(2).ToDateTimeString()
+	if r != e {
+		t.Fatalf("Expected %s, but got %s", e, r)
+	}
+}
+
+func TestCarbon_SubMinutes(t *testing.T) {
+	now := time.Now().Add(time.Minute * -2)
+	e := now.Format("2006-01-02 15:04:05")
+	r := New().Now().SubMinutes(2).ToDateTimeString()
+	if r != e {
+		t.Fatalf("Expected %s, but got %s", e, r)
+	}
+}
+
+func TestCarbon_AddMinute(t *testing.T) {
+	now := time.Now().Add(time.Minute)
+	e := now.Format("2006-01-02 15:04:05")
+	r := New().Now().AddMinute().ToDateTimeString()
+	if r != e {
+		t.Fatalf("Expected %s, but got %s", e, r)
+	}
+}
+
+func TestCarbon_SubMinute(t *testing.T) {
+	now := time.Now().Add(-time.Minute)
+	e := now.Format("2006-01-02 15:04:05")
+	r := New().Now().SubMinute().ToDateTimeString()
+	if r != e {
+		t.Fatalf("Expected %s, but got %s", e, r)
+	}
+}
+
+func TestCarbon_AddSeconds(t *testing.T) {
+	now := time.Now().Add(time.Second * 2)
+	e := now.Format("2006-01-02 15:04:05")
+	r := New().Now().AddSeconds(2).ToDateTimeString()
+	if r != e {
+		t.Fatalf("Expected %s, but got %s", e, r)
+	}
+}
+
+func TestCarbon_SubSeconds(t *testing.T) {
+	now := time.Now().Add(time.Second * -2)
+	e := now.Format("2006-01-02 15:04:05")
+	r := New().Now().SubSeconds(2).ToDateTimeString()
+	if r != e {
+		t.Fatalf("Expected %s, but got %s", e, r)
+	}
+}
+
+func TestCarbon_AddSecond(t *testing.T) {
+	now := time.Now().Add(time.Second)
+	e := now.Format("2006-01-02 15:04:05")
+	r := New().Now().AddSecond().ToDateTimeString()
+	if r != e {
+		t.Fatalf("Expected %s, but got %s", e, r)
+	}
+}
+
+func TestCarbon_SubSecond(t *testing.T) {
+	now := time.Now().Add(-time.Second)
+	e := now.Format("2006-01-02 15:04:05")
+	r := New().Now().SubSecond().ToDateTimeString()
+	if r != e {
+		t.Fatalf("Expected %s, but got %s", e, r)
+	}
+}
+
 func TestCarbon_IsLeapYear(t *testing.T) {
 	year := time.Now().Year()
 	e := false
@@ -260,7 +521,3 @@ func TestCarbon_IsSunday(t *testing.T) {
 	}
 }
 
-func TestCarbon_StartOfDay(t *testing.T) {
-	a := New().Parse("2020-09-13 17:10:00").ToTimeEndString()
-	fmt.Println(a)
-}
