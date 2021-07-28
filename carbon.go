d91136ea5edd95d2b9a7f5789fdbfda348712377
gouguoyin <245629560@qq.com>
2021-07-28 09:17:58 +0800

    更新版本号到v1.4.5

--- carbon.go
+++ carbon.go
@@ -1,7 +1,7 @@
 // @Title carbon
 // @Description A simple, semantic and developer-friendly golang package for datetime
 // @Page github.com/golang-module/carbon
-// @Version v1.4.4
+// @Version v1.4.5
 // @Author gouguoyin
 // @Blog www.gouguoyin.cn
 // @Email contact@gouguoyin.cn
