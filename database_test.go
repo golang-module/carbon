9fd881a7017b08c85db7de32f695e54f99d7b106
gouguoyin <245629560@qq.com>
2021-08-06 10:19:37 +0800

    add unit test

---
+++ database_test.go
@@ -0,0 +1,36 @@
+package carbon
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCarbon_Scan(t *testing.T) {
+	c := NewCarbon()
+	err := c.Scan(time.Now())
+	assert.Nil(t, err)
+	assert.Equal(t, c.ToDateTimeString(), Now().ToDateTimeString())
+}
+
+func TestError_Scan(t *testing.T) {
+	c, v := NewCarbon(), "xxx"
+	err := c.Scan(v)
+	assert.Equal(t, err, fmt.Errorf("can not convert %v to carbon", v))
+}
+
+func TestCarbon_Value(t *testing.T) {
+	c := Now()
+	v, err := c.Value()
+	assert.Nil(t, err)
+	assert.Equal(t, v, c.Time)
+}
+
+func TestError_Value(t *testing.T) {
+	c := Parse("")
+	v, err := c.Value()
+	assert.Nil(t, err)
+	assert.Equal(t, v, nil)
+}
