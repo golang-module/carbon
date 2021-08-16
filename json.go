9f24f026b5c2da675f53adb6798419f5e5bea409
gouguoyin <245629560@qq.com>
2021-08-16 09:45:47 +0800

    add DateTime、Date、Time、Timestamp struct

--- json.go
+++ json.go
@@ -6,42 +6,216 @@ import (
 	"strconv"
 )
 
-// ToDateTimeString defines ToDateTimeString structure.
+// DateTime defines a DateTime struct.
+type DateTime struct {
+	Carbon
+}
+
+// Date defines a Date struct.
+type Date struct {
+	Carbon
+}
+
+// Time defines a Time struct.
+type Time struct {
+	Carbon
+}
+
+// Timestamp defines a Timestamp struct.
+type Timestamp struct {
+	Carbon
+}
+
+// TimestampWithSecond defines a TimestampWithSecond struct.
+type TimestampWithSecond struct {
+	Carbon
+}
+
+// TimestampWithMillisecond defines a TimestampWithMillisecond struct.
+type TimestampWithMillisecond struct {
+	Carbon
+}
+
+// TimestampWithMicrosecond defines a TimestampWithMicrosecond struct.
+type TimestampWithMicrosecond struct {
+	Carbon
+}
+
+// TimestampWithNanosecond defines a TimestampWithNanosecond struct.
+type TimestampWithNanosecond struct {
+	Carbon
+}
+
+// MarshalJSON implements the interface MarshalJSON for json.Marshal.
+func (t DateTime) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf(`"%s"`, t.ToDateTimeString())), nil
+}
+
+// UnmarshalJSON implements the interface UnmarshalJSON for json.Unmarshal.
+func (t *DateTime) UnmarshalJSON(b []byte) error {
+	c := Parse(string(bytes.Trim(b, `"`)))
+	if c.Error == nil {
+		*t = DateTime{c}
+	}
+	return nil
+}
+
+// MarshalJSON implements the interface MarshalJSON for json.Marshal.
+func (t Date) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf(`"%s"`, t.ToDateString())), nil
+}
+
+// UnmarshalJSON implements the interface UnmarshalJSON for json.Unmarshal.
+func (t *Date) UnmarshalJSON(b []byte) error {
+	c := Parse(string(bytes.Trim(b, `"`)))
+	if c.Error == nil {
+		*t = Date{c}
+	}
+	return nil
+}
+
+// MarshalJSON implements the interface MarshalJSON for json.Marshal.
+func (t Time) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf(`"%s"`, t.ToTimeString())), nil
+}
+
+// UnmarshalJSON implements the interface UnmarshalJSON for json.Unmarshal.
+func (t *Time) UnmarshalJSON(b []byte) error {
+	c := ParseByFormat(string(bytes.Trim(b, `"`)), "H:i:s")
+	if c.Error == nil {
+		*t = Time{c}
+	}
+	return nil
+}
+
+// MarshalJSON implements the interface MarshalJSON for json.Marshal.
+func (t Timestamp) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf(`%d`, t.Timestamp())), nil
+}
+
+// UnmarshalJSON implements the interface UnmarshalJSON for json.Unmarshal.
+func (t *Timestamp) UnmarshalJSON(b []byte) error {
+	ts, err := strconv.ParseInt(string(b), 10, 64)
+	if ts == 0 || err != nil {
+		return err
+	}
+	c := CreateFromTimestamp(ts)
+	if c.Error == nil {
+		*t = Timestamp{c}
+	}
+	return nil
+}
+
+// MarshalJSON implements the interface MarshalJSON for json.Marshal.
+func (t TimestampWithSecond) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf(`%d`, t.TimestampWithSecond())), nil
+}
+
+// UnmarshalJSON implements the interface UnmarshalJSON for json.Unmarshal.
+func (t *TimestampWithSecond) UnmarshalJSON(b []byte) error {
+	ts, err := strconv.ParseInt(string(b), 10, 64)
+	if ts == 0 || err != nil {
+		return err
+	}
+	c := CreateFromTimestamp(ts)
+	if c.Error == nil {
+		*t = TimestampWithSecond{c}
+	}
+	return nil
+}
+
+// MarshalJSON implements the interface MarshalJSON for json.Marshal.
+func (t TimestampWithMillisecond) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf(`%d`, t.TimestampWithMillisecond())), nil
+}
+
+// UnmarshalJSON implements the interface UnmarshalJSON for json.Unmarshal.
+func (t *TimestampWithMillisecond) UnmarshalJSON(b []byte) error {
+	ts, err := strconv.ParseInt(string(b), 10, 64)
+	if ts == 0 || err != nil {
+		return err
+	}
+	c := CreateFromTimestamp(ts)
+	if c.Error == nil {
+		*t = TimestampWithMillisecond{c}
+	}
+	return nil
+}
+
+// MarshalJSON implements the interface MarshalJSON for json.Marshal.
+func (t TimestampWithMicrosecond) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf(`%d`, t.TimestampWithMicrosecond())), nil
+}
+
+// UnmarshalJSON implements the interface UnmarshalJSON for json.Unmarshal.
+func (t *TimestampWithMicrosecond) UnmarshalJSON(b []byte) error {
+	ts, err := strconv.ParseInt(string(b), 10, 64)
+	if ts == 0 || err != nil {
+		return err
+	}
+	c := CreateFromTimestamp(ts)
+	if c.Error == nil {
+		*t = TimestampWithMicrosecond{c}
+	}
+	return nil
+}
+
+// MarshalJSON implements the interface MarshalJSON for json.Marshal.
+func (t TimestampWithNanosecond) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf(`%d`, t.TimestampWithNanosecond())), nil
+}
+
+// UnmarshalJSON implements the interface UnmarshalJSON for json.Unmarshal.
+func (t *TimestampWithNanosecond) UnmarshalJSON(b []byte) error {
+	ts, err := strconv.ParseInt(string(b), 10, 64)
+	if ts == 0 || err != nil {
+		return err
+	}
+	c := CreateFromTimestamp(ts)
+	if c.Error == nil {
+		*t = TimestampWithNanosecond{c}
+	}
+	return nil
+}
+
+/* The following will be removed from v2.0 and only the above will be retained */
+
+// ToDateTimeString defines a ToDateTimeString struct.
 type ToDateTimeString struct {
 	Carbon
 }
 
-// ToDateString defines ToDateTimeString structure.
+// ToDateString defines a ToDateString struct.
 type ToDateString struct {
 	Carbon
 }
 
-// ToTimeString defines ToTimeString structure.
+// ToTimeString defines a ToTimeString struct.
 type ToTimeString struct {
 	Carbon
 }
 
-// ToTimestamp defines ToTimestamp structure.
+// ToTimestamp defines a ToTimestamp struct.
 type ToTimestamp struct {
 	Carbon
 }
 
-// ToTimestampWithSecond defines ToTimestampWithSecond structure.
+// ToTimestampWithSecond defines a ToTimestampWithSecond struct.
 type ToTimestampWithSecond struct {
 	Carbon
 }
 
-// ToTimestampWithMillisecond defines ToTimestampWithMillisecond structure.
+// ToTimestampWithMillisecond defines a ToTimestampWithMillisecond struct.
 type ToTimestampWithMillisecond struct {
 	Carbon
 }
 
-// ToTimestampWithMicrosecond defines ToTimestampWithMicrosecond structure.
+// ToTimestampWithMicrosecond defines a ToTimestampWithMicrosecond struct.
 type ToTimestampWithMicrosecond struct {
 	Carbon
 }
 
-// ToTimestampWithNanosecond defines ToTimestampWithNanosecond structure.
+// ToTimestampWithNanosecond defines a ToTimestampWithNanosecond struct.
 type ToTimestampWithNanosecond struct {
 	Carbon
 }
