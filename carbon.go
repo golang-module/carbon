ec171331fd092f8b4d8c2db3f1e7616662d55f7f
gouguoyin <245629560@qq.com>
2022-04-13 00:11:41 +0800

    Updated version from 2.0.1 to 2.1.0 and removed useless constants

--- carbon.go
+++ carbon.go
@@ -1,7 +1,7 @@
 // @Package carbon
 // @Description a simple, semantic and developer-friendly golang package for datetime
 // @Page github.com/golang-module/carbon
-// @Version v2.0.1
+// @Version v2.1.0
 // @Author gouguoyin
 // @Blog www.gouguoyin.cn
 // @Email contact@gouguoyin.cn
@@ -94,57 +94,63 @@ const (
 // numbers constant
 // 数字常量
 const (
-	YearsPerMillennium         = 1000    // 每千年1000年
-	YearsPerCentury            = 100     // 每世纪100年
-	YearsPerDecade             = 10      // 每十年10年
-	QuartersPerYear            = 4       // 每年4季度
-	MonthsPerYear              = 12      // 每年12月
-	MonthsPerQuarter           = 3       // 每季度3月
-	WeeksPerNormalYear         = 52      // 每常规年52周
-	weeksPerLongYear           = 53      // 每长年53周
-	WeeksPerMonth              = 4       // 每月4周
-	DaysPerLeapYear            = 366     // 每闰年366天
-	DaysPerNormalYear          = 365     // 每常规年365天
-	DaysPerWeek                = 7       // 每周7天
-	HoursPerWeek               = 168     // 每周168小时
-	HoursPerDay                = 24      // 每天24小时
-	MinutesPerDay              = 1440    // 每天1440分钟
-	MinutesPerHour             = 60      // 每小时60分钟
-	SecondsPerWeek             = 604800  // 每周604800秒
-	SecondsPerDay              = 86400   // 每天86400秒
-	SecondsPerHour             = 3600    // 每小时3600秒
-	SecondsPerMinute           = 60      // 每分钟60秒
-	MillisecondsPerSecond      = 1000    // 每秒1000毫秒
-	MicrosecondsPerMillisecond = 1000    // 每毫秒1000微秒
-	MicrosecondsPerSecond      = 1000000 // 每秒1000000微秒
+	YearsPerMillennium = 1000   // 每千年1000年
+	YearsPerCentury    = 100    // 每世纪100年
+	YearsPerDecade     = 10     // 每十年10年
+	QuartersPerYear    = 4      // 每年4个季度
+	MonthsPerYear      = 12     // 每年12月
+	MonthsPerQuarter   = 3      // 每季度3月
+	WeeksPerNormalYear = 52     // 每常规年52周
+	weeksPerLongYear   = 53     // 每长年53周
+	WeeksPerMonth      = 4      // 每月4周
+	DaysPerLeapYear    = 366    // 每闰年366天
+	DaysPerNormalYear  = 365    // 每常规年365天
+	DaysPerWeek        = 7      // 每周7天
+	HoursPerWeek       = 168    // 每周168小时
+	HoursPerDay        = 24     // 每天24小时
+	MinutesPerDay      = 1440   // 每天1440分钟
+	MinutesPerHour     = 60     // 每小时60分钟
+	SecondsPerWeek     = 604800 // 每周604800秒
+	SecondsPerDay      = 86400  // 每天86400秒
+	SecondsPerHour     = 3600   // 每小时3600秒
+	SecondsPerMinute   = 60     // 每分钟60秒
 )
 
 // formats constant
 // 时间格式化常量
 const (
-	AnsicFormat         = time.ANSIC
-	UnixDateFormat      = time.UnixDate
-	RubyDateFormat      = time.RubyDate
-	RFC822Format        = time.RFC822
-	RFC822ZFormat       = time.RFC822Z
-	RFC850Format        = time.RFC850
-	RFC1123Format       = time.RFC1123
-	RFC1123ZFormat      = time.RFC1123Z
-	RssFormat           = time.RFC1123Z
-	RFC2822Format       = time.RFC1123Z
-	RFC3339Format       = time.RFC3339
-	KitchenFormat       = time.Kitchen
-	CookieFormat        = "Monday, 02-Jan-2006 15:04:05 MST"
-	ISO8601Format       = "2006-01-02T15:04:05-07:00"
-	RFC1036Format       = "Mon, 02 Jan 06 15:04:05 -0700"
-	RFC7231Format       = "Mon, 02 Jan 2006 15:04:05 GMT"
-	DayDateTimeFormat   = "Mon, Jan 2, 2006 3:04 PM"
-	DateTimeFormat      = "2006-01-02 15:04:05"
-	DateFormat          = "2006-01-02"
-	TimeFormat          = "15:04:05"
-	ShortDateTimeFormat = "20060102150405"
-	ShortDateFormat     = "20060102"
-	ShortTimeFormat     = "150405"
+	AnsicFormat              = time.ANSIC
+	UnixDateFormat           = time.UnixDate
+	RubyDateFormat           = time.RubyDate
+	RFC822Format             = time.RFC822
+	RFC822ZFormat            = time.RFC822Z
+	RFC850Format             = time.RFC850
+	RFC1123Format            = time.RFC1123
+	RFC1123ZFormat           = time.RFC1123Z
+	RssFormat                = time.RFC1123Z
+	KitchenFormat            = time.Kitchen
+	RFC2822Format            = time.RFC1123Z
+	RFC3339Format            = "2006-01-02T15:04:05Z07:00"
+	RFC3339MilliFormat       = "2006-01-02T15:04:05.999Z07:00"
+	RFC3339MicroFormat       = "2006-01-02T15:04:05.999999Z07:00"
+	RFC3339NanoFormat        = "2006-01-02T15:04:05.999999999Z07:00"
+	CookieFormat             = "Monday, 02-Jan-2006 15:04:05 MST"
+	ISO8601Format            = "2006-01-02T15:04:05-07:00"
+	RFC1036Format            = "Mon, 02 Jan 06 15:04:05 -0700"
+	RFC7231Format            = "Mon, 02 Jan 2006 15:04:05 GMT"
+	DayDateTimeFormat        = "Mon, Jan 2, 2006 3:04 PM"
+	DateTimeFormat           = "2006-01-02 15:04:05"
+	DateTimeMilliFormat      = "2006-01-02 15:04:05.999"
+	DateTimeMicroFormat      = "2006-01-02 15:04:05.999999"
+	DateTimeNanoFormat       = "2006-01-02 15:04:05.999999999"
+	DateFormat               = "2006-01-02"
+	TimeFormat               = "15:04:05"
+	ShortDateTimeFormat      = "20060102150405"
+	ShortDateTimeMilliFormat = "20060102150405.999"
+	ShortDateTimeMicroFormat = "20060102150405.999999"
+	ShortDateTimeNanoFormat  = "20060102150405.999999999"
+	ShortDateFormat          = "20060102"
+	ShortTimeFormat          = "150405"
 )
 
 // Carbon defines a Carbon struct.
