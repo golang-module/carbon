ba53e0d024f96a2f61083641c365aba62a15cd6c
gouguoyin <245629560@qq.com>
2022-04-17 20:13:42 +0800

    update version from 1.6.1 to 1.6.2

--- carbon.go
+++ carbon.go
@@ -1,7 +1,7 @@
 // @Package carbon
 // @Description a simple, semantic and developer-friendly golang package for datetime
 // @Page github.com/golang-module/carbon
-// @Version v1.6.1
+// @Version v1.6.2
 // @Author gouguoyin
 // @Blog www.gouguoyin.cn
 // @Email contact@gouguoyin.cn
@@ -119,7 +119,7 @@ const (
 // formats constant
 // 时间格式化常量
 const (
-	ANSICFormat              = time.ANSIC
+	ANSICSFormat             = time.ANSIC
 	UnixDateFormat           = time.UnixDate
 	RubyDateFormat           = time.RubyDate
 	RFC822Format             = time.RFC822
