78424a54117871bc78a83a8ec9c612992bb72860
gouguoyin <245629560@qq.com>
2022-05-01 07:16:59 +0800

    v1.6.5 remove some structors

--- json.go
+++ json.go
@@ -16,18 +16,8 @@ type Date struct {
 	Carbon
 }
 
-// TimestampWithSecond defines a TimestampWithSecond struct, it will be replaced by Timestamp.
-type TimestampWithSecond struct {
-	Carbon
-}
-
 // Timestamp defines a Timestamp struct.
 type Timestamp struct {
-	Carbon
-}
-
-// TimestampWithMillisecond defines a TimestampWithMillisecond struct, it will be replaced by TimestampMilli.
-type TimestampWithMillisecond struct {
 	Carbon
 }
 
@@ -36,18 +26,8 @@ type TimestampMilli struct {
 	Carbon
 }
 
-// TimestampWithMicrosecond defines a TimestampWithMicrosecond struct, it will be replaced by TimestampMicro.
-type TimestampWithMicrosecond struct {
-	Carbon
-}
-
 // TimestampMicro defines a TimestampMicro struct.
 type TimestampMicro struct {
-	Carbon
-}
-
-// TimestampWithNanosecond defines a TimestampWithNanosecond struct, it will be replaced by TimestampNano.
-type TimestampWithNanosecond struct {
 	Carbon
 }
 
@@ -56,12 +36,12 @@ type TimestampNano struct {
 	Carbon
 }
 
-// MarshalJSON implements the interface MarshalJSON for json.Marshal.
+// MarshalJSON implements the interface json.Marshal for Date struct.
 func (t DateTime) MarshalJSON() ([]byte, error) {
 	return []byte(fmt.Sprintf(`"%s"`, t.ToDateTimeString())), nil
 }
 
-// UnmarshalJSON implements the interface UnmarshalJSON for json.Unmarshal.
+// UnmarshalJSON implements the interface json.Unmarshal for Date struct.
 func (t *DateTime) UnmarshalJSON(b []byte) error {
 	c := Parse(string(bytes.Trim(b, `"`)))
 	if c.Error == nil {
@@ -70,12 +50,12 @@ func (t *DateTime) UnmarshalJSON(b []byte) error {
 	return nil
 }
 
-// MarshalJSON implements the interface MarshalJSON for json.Marshal.
+// MarshalJSON implements the interface json.Marshal for Date struct.
 func (t Date) MarshalJSON() ([]byte, error) {
 	return []byte(fmt.Sprintf(`"%s"`, t.ToDateString())), nil
 }
 
-// UnmarshalJSON implements the interface UnmarshalJSON for json.Unmarshal.
+// UnmarshalJSON implements the interface json.Unmarshal for Date struct.
 func (t *Date) UnmarshalJSON(b []byte) error {
 	c := Parse(string(bytes.Trim(b, `"`)))
 	if c.Error == nil {
@@ -84,17 +64,14 @@ func (t *Date) UnmarshalJSON(b []byte) error {
 	return nil
 }
 
-// MarshalJSON implements the interface MarshalJSON for json.Marshal.
+// MarshalJSON implements the interface json.Marshal for Timestamp struct.
 func (t Timestamp) MarshalJSON() ([]byte, error) {
 	return []byte(fmt.Sprintf(`%d`, t.Timestamp())), nil
 }
 
-// UnmarshalJSON implements the interface UnmarshalJSON for json.Unmarshal.
+// UnmarshalJSON implements the interface json.Unmarshal for Timestamp struct.
 func (t *Timestamp) UnmarshalJSON(b []byte) error {
-	ts, err := strconv.ParseInt(string(b), 10, 64)
-	if ts == 0 || err != nil {
-		return err
-	}
+	ts, _ := strconv.ParseInt(string(b), 10, 64)
 	c := CreateFromTimestamp(ts)
 	if c.Error == nil {
 		*t = Timestamp{c}
@@ -102,53 +79,14 @@ func (t *Timestamp) UnmarshalJSON(b []byte) error {
 	return nil
 }
 
-// MarshalJSON implements the interface MarshalJSON for json.Marshal.
-func (t TimestampWithSecond) MarshalJSON() ([]byte, error) {
-	return []byte(fmt.Sprintf(`%d`, t.Timestamp())), nil
-}
-
-// UnmarshalJSON implements the interface UnmarshalJSON for json.Unmarshal.
-func (t *TimestampWithSecond) UnmarshalJSON(b []byte) error {
-	ts, err := strconv.ParseInt(string(b), 10, 64)
-	if ts == 0 || err != nil {
-		return err
-	}
-	c := CreateFromTimestamp(ts)
-	if c.Error == nil {
-		*t = TimestampWithSecond{c}
-	}
-	return nil
-}
-
-// MarshalJSON implements the interface MarshalJSON for json.Marshal.
-func (t TimestampWithMillisecond) MarshalJSON() ([]byte, error) {
-	return []byte(fmt.Sprintf(`%d`, t.TimestampMilli())), nil
-}
-
-// UnmarshalJSON implements the interface UnmarshalJSON for json.Unmarshal.
-func (t *TimestampWithMillisecond) UnmarshalJSON(b []byte) error {
-	ts, err := strconv.ParseInt(string(b), 10, 64)
-	if ts == 0 || err != nil {
-		return err
-	}
-	c := CreateFromTimestampMilli(ts)
-	if c.Error == nil {
-		*t = TimestampWithMillisecond{c}
-	}
-	return nil
-}
-
-// MarshalJSON implements the interface MarshalJSON for json.Marshal.
+// MarshalJSON implements the interface json.Marshal for TimestampMilli struct.
 func (t TimestampMilli) MarshalJSON() ([]byte, error) {
 	return []byte(fmt.Sprintf(`%d`, t.TimestampMilli())), nil
 }
 
-// UnmarshalJSON implements the interface UnmarshalJSON for json.Unmarshal.
+// UnmarshalJSON implements the interface json.Unmarshal for TimestampMilli struct.
 func (t *TimestampMilli) UnmarshalJSON(b []byte) error {
-	ts, err := strconv.ParseInt(string(b), 10, 64)
-	if ts == 0 || err != nil {
-		return err
-	}
+	ts, _ := strconv.ParseInt(string(b), 10, 64)
 	c := CreateFromTimestampMilli(ts)
 	if c.Error == nil {
 		*t = TimestampMilli{c}
@@ -156,35 +94,14 @@ func (t *TimestampMilli) UnmarshalJSON(b []byte) error {
 	return nil
 }
 
-// MarshalJSON implements the interface MarshalJSON for json.Marshal.
-func (t TimestampWithMicrosecond) MarshalJSON() ([]byte, error) {
-	return []byte(fmt.Sprintf(`%d`, t.TimestampMicro())), nil
-}
-
-// UnmarshalJSON implements the interface UnmarshalJSON for json.Unmarshal.
-func (t *TimestampWithMicrosecond) UnmarshalJSON(b []byte) error {
-	ts, err := strconv.ParseInt(string(b), 10, 64)
-	if ts == 0 || err != nil {
-		return err
-	}
-	c := CreateFromTimestampMicro(ts)
-	if c.Error == nil {
-		*t = TimestampWithMicrosecond{c}
-	}
-	return nil
-}
-
-// MarshalJSON implements the interface MarshalJSON for json.Marshal.
+// MarshalJSON implements the interface MarshalJSON for TimestampMicro struct.
 func (t TimestampMicro) MarshalJSON() ([]byte, error) {
 	return []byte(fmt.Sprintf(`%d`, t.TimestampMicro())), nil
 }
 
-// UnmarshalJSON implements the interface UnmarshalJSON for json.Unmarshal.
+// UnmarshalJSON implements the interface json.Unmarshal for TimestampMicro struct.
 func (t *TimestampMicro) UnmarshalJSON(b []byte) error {
-	ts, err := strconv.ParseInt(string(b), 10, 64)
-	if ts == 0 || err != nil {
-		return err
-	}
+	ts, _ := strconv.ParseInt(string(b), 10, 64)
 	c := CreateFromTimestampMicro(ts)
 	if c.Error == nil {
 		*t = TimestampMicro{c}
@@ -192,30 +109,9 @@ func (t *TimestampMicro) UnmarshalJSON(b []byte) error {
 	return nil
 }
 
-// MarshalJSON implements the interface MarshalJSON for json.Marshal.
-func (t TimestampWithNanosecond) MarshalJSON() ([]byte, error) {
-	return []byte(fmt.Sprintf(`%d`, t.TimestampNano())), nil
-}
-
-// UnmarshalJSON implements the interface UnmarshalJSON for json.Unmarshal.
-func (t *TimestampWithNanosecond) UnmarshalJSON(b []byte) error {
-	ts, err := strconv.ParseInt(string(b), 10, 64)
-	if ts == 0 || err != nil {
-		return err
-	}
-	c := CreateFromTimestampNano(ts)
-	if c.Error == nil {
-		*t = TimestampWithNanosecond{c}
-	}
-	return nil
-}
-
-// UnmarshalJSON implements the interface UnmarshalJSON for json.Unmarshal.
+// UnmarshalJSON implements the interface json.Unmarshal for TimestampNano struct.
 func (t *TimestampNano) UnmarshalJSON(b []byte) error {
-	ts, err := strconv.ParseInt(string(b), 10, 64)
-	if ts == 0 || err != nil {
-		return err
-	}
+	ts, _ := strconv.ParseInt(string(b), 10, 64)
 	c := CreateFromTimestampNano(ts)
 	if c.Error == nil {
 		*t = TimestampNano{c}
@@ -223,7 +119,7 @@ func (t *TimestampNano) UnmarshalJSON(b []byte) error {
 	return nil
 }
 
-// MarshalJSON implements the interface MarshalJSON for json.Marshal.
+// MarshalJSON implements the interface json.Marshal for TimestampNano struct.
 func (t TimestampNano) MarshalJSON() ([]byte, error) {
 	return []byte(fmt.Sprintf(`%d`, t.TimestampNano())), nil
 }
--- json_test.go
+++ json_test.go
@@ -8,18 +8,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-type Person1 struct {
-	Name        string                   `json:"name"`
-	Age         int                      `json:"age"`
-	Birthday    DateTime                 `json:"birthday"`
-	GraduatedAt Date                     `json:"graduated_at"`
-	DateTime1   TimestampWithSecond      `json:"date_time1"`
-	DateTime2   TimestampWithMillisecond `json:"date_time2"`
-	DateTime3   TimestampWithMicrosecond `json:"date_time3"`
-	DateTime4   TimestampWithNanosecond  `json:"date_time4"`
-}
-
-type Person2 struct {
+type Person struct {
 	Name        string         `json:"name"`
 	Age         int            `json:"age"`
 	Birthday    DateTime       `json:"birthday"`
@@ -30,30 +19,8 @@ type Person2 struct {
 	DateTime4   TimestampNano  `json:"date_time4"`
 }
 
-func TestCarbon_MarshalJSON1(t *testing.T) {
-	assert := assert.New(t)
-
-	person := Person1{
-		Name:        "gouguoyin",
-		Age:         18,
-		Birthday:    DateTime{Now().SubYears(18)},
-		GraduatedAt: Date{Parse("2020-08-05 13:14:15")},
-		DateTime1:   TimestampWithSecond{Parse("2023-08-05 13:14:15")},
-		DateTime2:   TimestampWithMillisecond{Parse("2024-08-05 13:14:15")},
-		DateTime3:   TimestampWithMicrosecond{Parse("2025-08-05 13:14:15")},
-		DateTime4:   TimestampWithNanosecond{Parse("2025-08-05 13:14:15")},
-	}
-	data, err := json.Marshal(&person)
-	if err != nil {
-		assert.NotNil(err)
-	}
-	fmt.Printf("Person output by json:\n%s\n", data)
-}
-
-func TestCarbon_MarshalJSON2(t *testing.T) {
-	assert := assert.New(t)
-
-	person := Person2{
+func TestCarbon_MarshalJSON(t *testing.T) {
+	person := Person{
 		Name:        "gouguoyin",
 		Age:         18,
 		Birthday:    DateTime{Now().SubYears(18)},
@@ -65,17 +32,14 @@ func TestCarbon_MarshalJSON2(t *testing.T) {
 	}
 	data, err := json.Marshal(&person)
 	if err != nil {
-		assert.NotNil(err)
+		assert.NotNil(t, err)
 	}
 	fmt.Printf("Person output by json:\n%s\n", data)
 }
 
-func TestCarbon_UnmarshalJSON1(t *testing.T) {
-	assert := assert.New(t)
-
-	person := new(Person1)
-
-	str1 := `{
+func TestCarbon_UnmarshalJSON(t *testing.T) {
+	person := new(Person)
+	str := `{
 		"name": "gouguoyin",
 		"age": 18,
 		"birthday": "2003-07-16 16:22:02",
@@ -86,64 +50,29 @@ func TestCarbon_UnmarshalJSON1(t *testing.T) {
 		"date_time4": 1754370855000000000
 	}`
 
-	err1 := json.Unmarshal([]byte(str1), &person)
-	if err1 != nil {
-		assert.NotNil(err1)
+	err := json.Unmarshal([]byte(str), &person)
+	if err != nil {
+		assert.NotNil(t, err)
 	}
-	fmt.Printf("Json string parse to person1:\n%+v\n", *person)
+	fmt.Printf("Json string parse to person:\n%+v\n", *person)
 
-	str2 := `{
-		"name": "",
-		"age": 0,
-		"birthday": "",
-		"graduated_at": "",
-		"date_time1": 0,
-		"date_time2": 0,
-		"date_time3": 0,
-		"date_time4": 0
-	}`
-	err2 := json.Unmarshal([]byte(str2), &person)
-	if err2 != nil {
-		assert.NotNil(err2)
-	}
-	fmt.Printf("Json string parse to person2:\n%+v\n", *person)
 }
 
-func TestCarbon_UnmarshalJSON2(t *testing.T) {
-	assert := assert.New(t)
-
-	person := new(Person2)
-
-	str1 := `{
-		"name": "gouguoyin",
-		"age": 18,
-		"birthday": "2003-07-16 16:22:02",
-		"graduated_at": "2020-08-05",
-		"date_time1": 1691212455,
-		"date_time2": 1722834855000,
-		"date_time3": 1754370855000000,
-		"date_time4": 1754370855000000000
-	}`
-
-	err1 := json.Unmarshal([]byte(str1), &person)
-	if err1 != nil {
-		assert.NotNil(err1)
-	}
-	fmt.Printf("Json string parse to person1:\n%+v\n", *person)
-
-	str2 := `{
+func TestError_Json(t *testing.T) {
+	person := new(Person)
+	str := `{
 		"name": "",
 		"age": 0,
 		"birthday": "",
-		"graduated_at": "",
+		"graduated_at": "xxx",
 		"date_time1": 0,
 		"date_time2": 0,
 		"date_time3": 0,
 		"date_time4": 0
 	}`
-	err2 := json.Unmarshal([]byte(str2), &person)
-	if err2 != nil {
-		assert.NotNil(err2)
+	err := json.Unmarshal([]byte(str), &person)
+	if err != nil {
+		assert.NotNil(t, err)
 	}
-	fmt.Printf("Json string parse to person2:\n%+v\n", *person)
+	fmt.Printf("Json string parse to person:\n%+v\n", *person)
 }
