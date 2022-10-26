57661e4d6912bf72d44e1d44311670f55cfc30bc
gouguoyin <245629560@qq.com>
2022-10-27 01:02:38 +0800

    Updated error message

--- errors.go
+++ errors.go
@@ -25,17 +25,17 @@ var invalidDurationError = func(duration string) error {
 // returns an invalid value error.
 // 无效的时间字符串错误
 var invalidValueError = func(value string) error {
-	return fmt.Errorf("cannot parse %q as carbon, please make sure the value is valid", value)
+	return fmt.Errorf("cannot parse string %q as carbon, please make sure the value is valid", value)
 }
 
 // returns an invalid layout error.
 // 无效的布局模板错误
 var invalidLayoutError = func(value, layout string) error {
-	return fmt.Errorf("cannot parse %q as carbon by layout %q, please make sure the value and layout match", value, layout)
+	return fmt.Errorf("cannot parse string %q as carbon by layout %q, please make sure the value and layout match", value, layout)
 }
 
 // returns an invalid format error.
 // 无效的格式模板错误
 var invalidFormatError = func(value, format string) error {
-	return fmt.Errorf("cannot parse %q as carbon by format %q, please make sure the value and format match", value, format)
+	return fmt.Errorf("cannot parse string %q as carbon by format %q, please make sure the value and format match", value, format)
 }
