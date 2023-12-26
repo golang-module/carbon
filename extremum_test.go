0ddfa09bff5b78a65132971dd078513bd3b84ea3
Peleus <245629560@qq.com>
2023-12-26 17:28:52 +0800

    Move `Closest` and `Farthest` methods test case from `traveler_test.go` to `extremum_test.go`

---
+++ extremum_test.go
@@ -0,0 +1,58 @@
+package carbon
+
+import (
+	"github.com/stretchr/testify/assert"
+	"strconv"
+	"testing"
+)
+
+func TestCarbon_Closest(t *testing.T) {
+	assert := assert.New(t)
+
+	tests := []struct {
+		input1   string
+		input2   string
+		input3   string
+		expected string
+	}{
+		{"", "2023-03-28", "2023-04-16", "2023-03-28"},
+		{"2023-04-01", "", "2023-04-16", "2023-04-16"},
+		{"2023-04-01", "2023-03-28", "", "2023-03-28"},
+		{"2023-04-01", "", "", ""},
+
+		{"2023-04-01", "2023-03-28", "2023-03-28", "2023-03-28"},
+		{"2023-04-01", "2023-03-28", "2023-04-16", "2023-03-28"},
+	}
+
+	for index, test := range tests {
+		c := Parse(test.input1).Closest(Parse(test.input2), Parse(test.input3))
+		assert.Nil(c.Error)
+		assert.Equal(test.expected, c.ToDateString(), "Current test index is "+strconv.Itoa(index))
+	}
+}
+
+func TestCarbon_Farthest(t *testing.T) {
+	assert := assert.New(t)
+
+	tests := []struct {
+		input1   string
+		input2   string
+		input3   string
+		expected string
+	}{
+		{"", "2023-03-28", "2023-04-16", "2023-04-16"},
+		{"2023-04-01", "", "2023-04-16", "2023-04-16"},
+		{"2023-04-01", "2023-03-28", "", "2023-03-28"},
+		{"2023-04-01", "", "", ""},
+
+		{"2023-04-01", "2023-03-28", "2023-03-28", "2023-03-28"},
+		{"2023-04-01", "2023-03-28", "2023-04-16", "2023-04-16"},
+		{"2023-04-01", "2023-04-05", "2023-04-02", "2023-04-05"},
+	}
+
+	for index, test := range tests {
+		c := Parse(test.input1).Farthest(Parse(test.input2), Parse(test.input3))
+		assert.Nil(c.Error)
+		assert.Equal(test.expected, c.ToDateString(), "Current test index is "+strconv.Itoa(index))
+	}
+}
