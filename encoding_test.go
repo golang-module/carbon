6e9857dc2b3fbb429f79e531a425bbc7d9e626d9
Peleus <245629560@qq.com>
2023-12-26 22:57:33 +0800

    Fix bug

--- encoding_test.go
+++ encoding_test.go
@@ -12,64 +12,70 @@ type Person struct {
 	Name string `json:"name"`
 	Age  int    `json:"age"`
 
-	Birthday0 Carbon `json:"birthday0" tz:"PRC"`
+	Birthday0 Carbon `json:"birthday0"`
 
+	Birthday1 Carbon `json:"birthday1" carbon:"layout:2006-01-02"`
+	Birthday2 Carbon `json:"birthday2" carbon:"layout:15:04:05"`
+	Birthday3 Carbon `json:"birthday3" carbon:"layout:2006-01-02 15:04:05"`
 	Birthday4 Carbon `json:"birthday4" carbon:"layout:2006-01-02" tz:"PRC"`
 	Birthday5 Carbon `json:"birthday5" carbon:"layout:15:04:05" tz:"PRC"`
 	Birthday6 Carbon `json:"birthday6" carbon:"layout:2006-01-02 15:04:05" tz:"PRC"`
 
+	Birthday7  Carbon `json:"birthday7" carbon:"format:Y-m-d"`
+	Birthday8  Carbon `json:"birthday8" carbon:"format:H:i:s"`
+	Birthday9  Carbon `json:"birthday9" carbon:"format:Y-m-d H:i:s"`
 	Birthday10 Carbon `json:"birthday10" carbon:"format:Y-m-d" tz:"PRC"`
 	Birthday11 Carbon `json:"birthday11" carbon:"format:H:i:s" tz:"PRC"`
 	Birthday12 Carbon `json:"birthday12" carbon:"format:Y-m-d H:i:s" tz:"PRC"`
 
-	Birthday13 Carbon `json:"birthday13" carbon:"dateTime" tz:"PRC"`
-	Birthday14 Carbon `json:"birthday14" carbon:"dateTimeMilli" tz:"PRC"`
-	Birthday15 Carbon `json:"birthday15" carbon:"dateTimeMicro" tz:"PRC"`
-	Birthday16 Carbon `json:"birthday16" carbon:"dateTimeNano" tz:"PRC"`
-	Birthday17 Carbon `json:"birthday17" carbon:"shortDateTime" tz:"PRC"`
-	Birthday18 Carbon `json:"birthday18" carbon:"shortDateTimeMilli" tz:"PRC"`
-	Birthday19 Carbon `json:"birthday19" carbon:"shortDateTimeMicro" tz:"PRC"`
-	Birthday20 Carbon `json:"birthday20" carbon:"shortDateTimeNano" tz:"PRC"`
-	Birthday21 Carbon `json:"birthday21" carbon:"dayDateTime" tz:"PRC"`
-	Birthday22 Carbon `json:"birthday22" carbon:"date" tz:"PRC"`
-	Birthday23 Carbon `json:"birthday23" carbon:"dateMilli" tz:"PRC"`
-	Birthday24 Carbon `json:"birthday24" carbon:"dateMicro" tz:"PRC"`
-	Birthday25 Carbon `json:"birthday25" carbon:"dateNano" tz:"PRC"`
-	Birthday26 Carbon `json:"birthday26" carbon:"shortDate" tz:"PRC"`
-	Birthday27 Carbon `json:"birthday27" carbon:"shortDateMilli" tz:"PRC"`
-	Birthday28 Carbon `json:"birthday28" carbon:"shortDateMicro" tz:"PRC"`
-	Birthday29 Carbon `json:"birthday29" carbon:"shortDateNano" tz:"PRC"`
-	Birthday30 Carbon `json:"birthday30" carbon:"time" tz:"PRC"`
-	Birthday31 Carbon `json:"birthday31" carbon:"timeMilli" tz:"PRC"`
-	Birthday32 Carbon `json:"birthday32" carbon:"timeMicro" tz:"PRC"`
-	Birthday33 Carbon `json:"birthday33" carbon:"timeNano" tz:"PRC"`
-	Birthday34 Carbon `json:"birthday34" carbon:"shortTime" tz:"PRC"`
-	Birthday35 Carbon `json:"birthday35" carbon:"shortTimeMilli" tz:"PRC"`
-	Birthday36 Carbon `json:"birthday36" carbon:"shortTimeMicro" tz:"PRC"`
-	Birthday37 Carbon `json:"birthday37" carbon:"shortTimeNano" tz:"PRC"`
-	Birthday38 Carbon `json:"birthday38" carbon:"atom" tz:"PRC"`
-	Birthday39 Carbon `json:"birthday39" carbon:"ansic" tz:"PRC"`
-	Birthday40 Carbon `json:"birthday40" carbon:"cookie" tz:"PRC"`
-	Birthday41 Carbon `json:"birthday41" carbon:"kitchen" tz:"PRC"`
-	Birthday42 Carbon `json:"birthday42" carbon:"rss" tz:"PRC"`
-	Birthday43 Carbon `json:"birthday43" carbon:"rubyDate" tz:"PRC"`
-	Birthday44 Carbon `json:"birthday44" carbon:"unixDate" tz:"PRC"`
-	Birthday45 Carbon `json:"birthday45" carbon:"rfc1036" tz:"PRC"`
-	Birthday46 Carbon `json:"birthday46" carbon:"rfc1123" tz:"PRC"`
-	Birthday47 Carbon `json:"birthday47" carbon:"rfc1123Z" tz:"PRC"`
-	Birthday48 Carbon `json:"birthday48" carbon:"rfc2822" tz:"PRC"`
-	Birthday49 Carbon `json:"birthday49" carbon:"rfc3339" tz:"PRC"`
-	Birthday50 Carbon `json:"birthday50" carbon:"rfc3339Milli" tz:"PRC"`
-	Birthday51 Carbon `json:"birthday51" carbon:"rfc3339Micro" tz:"PRC"`
-	Birthday52 Carbon `json:"birthday52" carbon:"rfc3339Nano" tz:"PRC"`
-	Birthday53 Carbon `json:"birthday53" carbon:"rfc7231" tz:"PRC"`
-	Birthday54 Carbon `json:"birthday54" carbon:"rfc822" tz:"PRC"`
-	Birthday55 Carbon `json:"birthday55" carbon:"rfc822Z" tz:"PRC"`
-	Birthday56 Carbon `json:"birthday56" carbon:"rfc850" tz:"PRC"`
-	Birthday57 Carbon `json:"birthday57" carbon:"iso8601" tz:"PRC"`
-	Birthday58 Carbon `json:"birthday58" carbon:"iso8601Milli" tz:"PRC"`
-	Birthday59 Carbon `json:"birthday59" carbon:"iso8601Micro" tz:"PRC"`
-	Birthday60 Carbon `json:"birthday60" carbon:"iso8601Nano" tz:"PRC"`
+	Birthday13 Carbon `json:"birthday13" carbon:"dateTime"`
+	Birthday14 Carbon `json:"birthday14" carbon:"dateTimeMilli"`
+	Birthday15 Carbon `json:"birthday15" carbon:"dateTimeMicro"`
+	Birthday16 Carbon `json:"birthday16" carbon:"dateTimeNano"`
+	Birthday17 Carbon `json:"birthday17" carbon:"shortDateTime"`
+	Birthday18 Carbon `json:"birthday18" carbon:"shortDateTimeMilli"`
+	Birthday19 Carbon `json:"birthday19" carbon:"shortDateTimeMicro"`
+	Birthday20 Carbon `json:"birthday20" carbon:"shortDateTimeNano"`
+	Birthday21 Carbon `json:"birthday21" carbon:"dayDateTime"`
+	Birthday22 Carbon `json:"birthday22" carbon:"date"`
+	Birthday23 Carbon `json:"birthday23" carbon:"dateMilli"`
+	Birthday24 Carbon `json:"birthday24" carbon:"dateMicro"`
+	Birthday25 Carbon `json:"birthday25" carbon:"dateNano"`
+	Birthday26 Carbon `json:"birthday26" carbon:"shortDate"`
+	Birthday27 Carbon `json:"birthday27" carbon:"shortDateMilli"`
+	Birthday28 Carbon `json:"birthday28" carbon:"shortDateMicro"`
+	Birthday29 Carbon `json:"birthday29" carbon:"shortDateNano"`
+	Birthday30 Carbon `json:"birthday30" carbon:"time"`
+	Birthday31 Carbon `json:"birthday31" carbon:"timeMilli"`
+	Birthday32 Carbon `json:"birthday32" carbon:"timeMicro"`
+	Birthday33 Carbon `json:"birthday33" carbon:"timeNano"`
+	Birthday34 Carbon `json:"birthday34" carbon:"shortTime"`
+	Birthday35 Carbon `json:"birthday35" carbon:"shortTimeMilli"`
+	Birthday36 Carbon `json:"birthday36" carbon:"shortTimeMicro"`
+	Birthday37 Carbon `json:"birthday37" carbon:"shortTimeNano"`
+	Birthday38 Carbon `json:"birthday38" carbon:"atom"`
+	Birthday39 Carbon `json:"birthday39" carbon:"ansic"`
+	Birthday40 Carbon `json:"birthday40" carbon:"cookie"`
+	Birthday41 Carbon `json:"birthday41" carbon:"kitchen"`
+	Birthday42 Carbon `json:"birthday42" carbon:"rss"`
+	Birthday43 Carbon `json:"birthday43" carbon:"rubyDate"`
+	Birthday44 Carbon `json:"birthday44" carbon:"unixDate"`
+	Birthday45 Carbon `json:"birthday45" carbon:"rfc1036"`
+	Birthday46 Carbon `json:"birthday46" carbon:"rfc1123"`
+	Birthday47 Carbon `json:"birthday47" carbon:"rfc1123Z"`
+	Birthday48 Carbon `json:"birthday48" carbon:"rfc2822"`
+	Birthday49 Carbon `json:"birthday49" carbon:"rfc3339"`
+	Birthday50 Carbon `json:"birthday50" carbon:"rfc3339Milli"`
+	Birthday51 Carbon `json:"birthday51" carbon:"rfc3339Micro"`
+	Birthday52 Carbon `json:"birthday52" carbon:"rfc3339Nano"`
+	Birthday53 Carbon `json:"birthday53" carbon:"rfc7231"`
+	Birthday54 Carbon `json:"birthday54" carbon:"rfc822"`
+	Birthday55 Carbon `json:"birthday55" carbon:"rfc822Z"`
+	Birthday56 Carbon `json:"birthday56" carbon:"rfc850"`
+	Birthday57 Carbon `json:"birthday57" carbon:"iso8601"`
+	Birthday58 Carbon `json:"birthday58" carbon:"iso8601Milli"`
+	Birthday59 Carbon `json:"birthday59" carbon:"iso8601Micro"`
+	Birthday60 Carbon `json:"birthday60" carbon:"iso8601Nano"`
 }
 
 var c = Parse("2020-08-05 13:14:15.999999999", PRC)
@@ -77,9 +83,15 @@ var person = Person{
 	Name:       "gouguoyin",
 	Age:        18,
 	Birthday0:  c,
+	Birthday1:  c,
+	Birthday2:  c,
+	Birthday3:  c,
 	Birthday4:  c,
 	Birthday5:  c,
 	Birthday6:  c,
+	Birthday7:  c,
+	Birthday8:  c,
+	Birthday9:  c,
 	Birthday10: c,
 	Birthday11: c,
 	Birthday12: c,
@@ -141,9 +153,15 @@ func TestCarbon_MarshalJSON(t *testing.T) {
 	assert.Nil(t, marshalErr)
 
 	assert.Equal(t, "2020-08-05 13:14:15", person.Birthday0.String())
+	assert.Equal(t, "2020-08-05", person.Birthday1.String())
+	assert.Equal(t, "13:14:15", person.Birthday2.String())
+	assert.Equal(t, "2020-08-05 13:14:15", person.Birthday3.String())
 	assert.Equal(t, "2020-08-05", person.Birthday4.String())
 	assert.Equal(t, "13:14:15", person.Birthday5.String())
 	assert.Equal(t, "2020-08-05 13:14:15", person.Birthday6.String())
+	assert.Equal(t, "2020-08-05", person.Birthday7.String())
+	assert.Equal(t, "13:14:15", person.Birthday8.String())
+	assert.Equal(t, "2020-08-05 13:14:15", person.Birthday9.String())
 	assert.Equal(t, "2020-08-05", person.Birthday10.String())
 	assert.Equal(t, "13:14:15", person.Birthday11.String())
 	assert.Equal(t, "2020-08-05 13:14:15", person.Birthday12.String())
@@ -195,62 +213,6 @@ func TestCarbon_MarshalJSON(t *testing.T) {
 	assert.Equal(t, "2020-08-05T13:14:15.999+08:00", person.Birthday58.String())
 	assert.Equal(t, "2020-08-05T13:14:15.999999+08:00", person.Birthday59.String())
 	assert.Equal(t, "2020-08-05T13:14:15.999999999+08:00", person.Birthday60.String())
-
-	assert.Equal(t, "layout:2006-01-02 15:04:05;tz:PRC", person.Birthday0.Tag())
-	assert.Equal(t, "layout:2006-01-02;tz:PRC", person.Birthday4.Tag())
-	assert.Equal(t, "layout:15:04:05;tz:PRC", person.Birthday5.Tag())
-	assert.Equal(t, "layout:2006-01-02 15:04:05;tz:PRC", person.Birthday6.Tag())
-	assert.Equal(t, "format:Y-m-d;tz:PRC", person.Birthday10.Tag())
-	assert.Equal(t, "format:H:i:s;tz:PRC", person.Birthday11.Tag())
-	assert.Equal(t, "format:Y-m-d H:i:s;tz:PRC", person.Birthday12.Tag())
-	assert.Equal(t, "layout:2006-01-02 15:04:05;tz:PRC", person.Birthday13.Tag())
-	assert.Equal(t, "layout:2006-01-02 15:04:05.999;tz:PRC", person.Birthday14.Tag())
-	assert.Equal(t, "layout:2006-01-02 15:04:05.999999;tz:PRC", person.Birthday15.Tag())
-	assert.Equal(t, "layout:2006-01-02 15:04:05.999999999;tz:PRC", person.Birthday16.Tag())
-	assert.Equal(t, "layout:20060102150405;tz:PRC", person.Birthday17.Tag())
-	assert.Equal(t, "layout:20060102150405.999;tz:PRC", person.Birthday18.Tag())
-	assert.Equal(t, "layout:20060102150405.999999;tz:PRC", person.Birthday19.Tag())
-	assert.Equal(t, "layout:20060102150405.999999999;tz:PRC", person.Birthday20.Tag())
-	assert.Equal(t, "layout:Mon, Jan 2, 2006 3:04 PM;tz:PRC", person.Birthday21.Tag())
-	assert.Equal(t, "layout:2006-01-02;tz:PRC", person.Birthday22.Tag())
-	assert.Equal(t, "layout:2006-01-02.999;tz:PRC", person.Birthday23.Tag())
-	assert.Equal(t, "layout:2006-01-02.999999;tz:PRC", person.Birthday24.Tag())
-	assert.Equal(t, "layout:2006-01-02.999999999;tz:PRC", person.Birthday25.Tag())
-	assert.Equal(t, "layout:20060102;tz:PRC", person.Birthday26.Tag())
-	assert.Equal(t, "layout:20060102.999;tz:PRC", person.Birthday27.Tag())
-	assert.Equal(t, "layout:20060102.999999;tz:PRC", person.Birthday28.Tag())
-	assert.Equal(t, "layout:20060102.999999999;tz:PRC", person.Birthday29.Tag())
-	assert.Equal(t, "layout:15:04:05;tz:PRC", person.Birthday30.Tag())
-	assert.Equal(t, "layout:15:04:05.999;tz:PRC", person.Birthday31.Tag())
-	assert.Equal(t, "layout:15:04:05.999999;tz:PRC", person.Birthday32.Tag())
-	assert.Equal(t, "layout:15:04:05.999999999;tz:PRC", person.Birthday33.Tag())
-	assert.Equal(t, "layout:150405;tz:PRC", person.Birthday34.Tag())
-	assert.Equal(t, "layout:150405.999;tz:PRC", person.Birthday35.Tag())
-	assert.Equal(t, "layout:150405.999999;tz:PRC", person.Birthday36.Tag())
-	assert.Equal(t, "layout:150405.999999999;tz:PRC", person.Birthday37.Tag())
-	assert.Equal(t, "layout:2006-01-02T15:04:05Z07:00;tz:PRC", person.Birthday38.Tag())
-	assert.Equal(t, "layout:Mon Jan _2 15:04:05 2006;tz:PRC", person.Birthday39.Tag())
-	assert.Equal(t, "layout:Monday, 02-Jan-2006 15:04:05 MST;tz:PRC", person.Birthday40.Tag())
-	assert.Equal(t, "layout:3:04PM;tz:PRC", person.Birthday41.Tag())
-	assert.Equal(t, "layout:Mon, 02 Jan 2006 15:04:05 -0700;tz:PRC", person.Birthday42.Tag())
-	assert.Equal(t, "layout:Mon Jan 02 15:04:05 -0700 2006;tz:PRC", person.Birthday43.Tag())
-	assert.Equal(t, "layout:Mon Jan _2 15:04:05 MST 2006;tz:PRC", person.Birthday44.Tag())
-	assert.Equal(t, "layout:Mon, 02 Jan 06 15:04:05 -0700;tz:PRC", person.Birthday45.Tag())
-	assert.Equal(t, "layout:Mon, 02 Jan 2006 15:04:05 MST;tz:PRC", person.Birthday46.Tag())
-	assert.Equal(t, "layout:Mon, 02 Jan 2006 15:04:05 -0700;tz:PRC", person.Birthday47.Tag())
-	assert.Equal(t, "layout:Mon, 02 Jan 2006 15:04:05 -0700;tz:PRC", person.Birthday48.Tag())
-	assert.Equal(t, "layout:2006-01-02T15:04:05Z07:00;tz:PRC", person.Birthday49.Tag())
-	assert.Equal(t, "layout:2006-01-02T15:04:05.999Z07:00;tz:PRC", person.Birthday50.Tag())
-	assert.Equal(t, "layout:2006-01-02T15:04:05.999999Z07:00;tz:PRC", person.Birthday51.Tag())
-	assert.Equal(t, "layout:2006-01-02T15:04:05.999999999Z07:00;tz:PRC", person.Birthday52.Tag())
-	assert.Equal(t, "layout:Mon, 02 Jan 2006 15:04:05 MST;tz:PRC", person.Birthday53.Tag())
-	assert.Equal(t, "layout:02 Jan 06 15:04 MST;tz:PRC", person.Birthday54.Tag())
-	assert.Equal(t, "layout:02 Jan 06 15:04 -0700;tz:PRC", person.Birthday55.Tag())
-	assert.Equal(t, "layout:Monday, 02-Jan-06 15:04:05 MST;tz:PRC", person.Birthday56.Tag())
-	assert.Equal(t, "layout:2006-01-02T15:04:05-07:00;tz:PRC", person.Birthday57.Tag())
-	assert.Equal(t, "layout:2006-01-02T15:04:05.999-07:00;tz:PRC", person.Birthday58.Tag())
-	assert.Equal(t, "layout:2006-01-02T15:04:05.999999-07:00;tz:PRC", person.Birthday59.Tag())
-	assert.Equal(t, "layout:2006-01-02T15:04:05.999999999-07:00;tz:PRC", person.Birthday60.Tag())
 
 	fmt.Printf("person output by json:\n%s\n", data)
 }
@@ -329,9 +291,15 @@ func TestCarbon_UnmarshalJSON(t *testing.T) {
 	assert.Nil(t, unmarshalErr)
 
 	assert.Equal(t, "2020-08-05 13:14:15", person.Birthday0.String())
+	assert.Equal(t, "2020-08-05", person.Birthday1.String())
+	assert.Equal(t, "13:14:15", person.Birthday2.String())
+	assert.Equal(t, "2020-08-05 13:14:15", person.Birthday3.String())
 	assert.Equal(t, "2020-08-05", person.Birthday4.String())
 	assert.Equal(t, "13:14:15", person.Birthday5.String())
 	assert.Equal(t, "2020-08-05 13:14:15", person.Birthday6.String())
+	assert.Equal(t, "2020-08-05", person.Birthday7.String())
+	assert.Equal(t, "13:14:15", person.Birthday8.String())
+	assert.Equal(t, "2020-08-05 13:14:15", person.Birthday9.String())
 	assert.Equal(t, "2020-08-05", person.Birthday10.String())
 	assert.Equal(t, "13:14:15", person.Birthday11.String())
 	assert.Equal(t, "2020-08-05 13:14:15", person.Birthday12.String())
@@ -383,62 +351,6 @@ func TestCarbon_UnmarshalJSON(t *testing.T) {
 	assert.Equal(t, "2020-08-05T13:14:15.999+08:00", person.Birthday58.String())
 	assert.Equal(t, "2020-08-05T13:14:15.999999+08:00", person.Birthday59.String())
 	assert.Equal(t, "2020-08-05T13:14:15.999999999+08:00", person.Birthday60.String())
-
-	assert.Equal(t, "layout:2006-01-02 15:04:05;tz:PRC", person.Birthday0.Tag())
-	assert.Equal(t, "layout:2006-01-02;tz:PRC", person.Birthday4.Tag())
-	assert.Equal(t, "layout:15:04:05;tz:PRC", person.Birthday5.Tag())
-	assert.Equal(t, "layout:2006-01-02 15:04:05;tz:PRC", person.Birthday6.Tag())
-	assert.Equal(t, "format:Y-m-d;tz:PRC", person.Birthday10.Tag())
-	assert.Equal(t, "format:H:i:s;tz:PRC", person.Birthday11.Tag())
-	assert.Equal(t, "format:Y-m-d H:i:s;tz:PRC", person.Birthday12.Tag())
-	assert.Equal(t, "layout:2006-01-02 15:04:05;tz:PRC", person.Birthday13.Tag())
-	assert.Equal(t, "layout:2006-01-02 15:04:05.999;tz:PRC", person.Birthday14.Tag())
-	assert.Equal(t, "layout:2006-01-02 15:04:05.999999;tz:PRC", person.Birthday15.Tag())
-	assert.Equal(t, "layout:2006-01-02 15:04:05.999999999;tz:PRC", person.Birthday16.Tag())
-	assert.Equal(t, "layout:20060102150405;tz:PRC", person.Birthday17.Tag())
-	assert.Equal(t, "layout:20060102150405.999;tz:PRC", person.Birthday18.Tag())
-	assert.Equal(t, "layout:20060102150405.999999;tz:PRC", person.Birthday19.Tag())
-	assert.Equal(t, "layout:20060102150405.999999999;tz:PRC", person.Birthday20.Tag())
-	assert.Equal(t, "layout:Mon, Jan 2, 2006 3:04 PM;tz:PRC", person.Birthday21.Tag())
-	assert.Equal(t, "layout:2006-01-02;tz:PRC", person.Birthday22.Tag())
-	assert.Equal(t, "layout:2006-01-02.999;tz:PRC", person.Birthday23.Tag())
-	assert.Equal(t, "layout:2006-01-02.999999;tz:PRC", person.Birthday24.Tag())
-	assert.Equal(t, "layout:2006-01-02.999999999;tz:PRC", person.Birthday25.Tag())
-	assert.Equal(t, "layout:20060102;tz:PRC", person.Birthday26.Tag())
-	assert.Equal(t, "layout:20060102.999;tz:PRC", person.Birthday27.Tag())
-	assert.Equal(t, "layout:20060102.999999;tz:PRC", person.Birthday28.Tag())
-	assert.Equal(t, "layout:20060102.999999999;tz:PRC", person.Birthday29.Tag())
-	assert.Equal(t, "layout:15:04:05;tz:PRC", person.Birthday30.Tag())
-	assert.Equal(t, "layout:15:04:05.999;tz:PRC", person.Birthday31.Tag())
-	assert.Equal(t, "layout:15:04:05.999999;tz:PRC", person.Birthday32.Tag())
-	assert.Equal(t, "layout:15:04:05.999999999;tz:PRC", person.Birthday33.Tag())
-	assert.Equal(t, "layout:150405;tz:PRC", person.Birthday34.Tag())
-	assert.Equal(t, "layout:150405.999;tz:PRC", person.Birthday35.Tag())
-	assert.Equal(t, "layout:150405.999999;tz:PRC", person.Birthday36.Tag())
-	assert.Equal(t, "layout:150405.999999999;tz:PRC", person.Birthday37.Tag())
-	assert.Equal(t, "layout:2006-01-02T15:04:05Z07:00;tz:PRC", person.Birthday38.Tag())
-	assert.Equal(t, "layout:Mon Jan _2 15:04:05 2006;tz:PRC", person.Birthday39.Tag())
-	assert.Equal(t, "layout:Monday, 02-Jan-2006 15:04:05 MST;tz:PRC", person.Birthday40.Tag())
-	assert.Equal(t, "layout:3:04PM;tz:PRC", person.Birthday41.Tag())
-	assert.Equal(t, "layout:Mon, 02 Jan 2006 15:04:05 -0700;tz:PRC", person.Birthday42.Tag())
-	assert.Equal(t, "layout:Mon Jan 02 15:04:05 -0700 2006;tz:PRC", person.Birthday43.Tag())
-	assert.Equal(t, "layout:Mon Jan _2 15:04:05 MST 2006;tz:PRC", person.Birthday44.Tag())
-	assert.Equal(t, "layout:Mon, 02 Jan 06 15:04:05 -0700;tz:PRC", person.Birthday45.Tag())
-	assert.Equal(t, "layout:Mon, 02 Jan 2006 15:04:05 MST;tz:PRC", person.Birthday46.Tag())
-	assert.Equal(t, "layout:Mon, 02 Jan 2006 15:04:05 -0700;tz:PRC", person.Birthday47.Tag())
-	assert.Equal(t, "layout:Mon, 02 Jan 2006 15:04:05 -0700;tz:PRC", person.Birthday48.Tag())
-	assert.Equal(t, "layout:2006-01-02T15:04:05Z07:00;tz:PRC", person.Birthday49.Tag())
-	assert.Equal(t, "layout:2006-01-02T15:04:05.999Z07:00;tz:PRC", person.Birthday50.Tag())
-	assert.Equal(t, "layout:2006-01-02T15:04:05.999999Z07:00;tz:PRC", person.Birthday51.Tag())
-	assert.Equal(t, "layout:2006-01-02T15:04:05.999999999Z07:00;tz:PRC", person.Birthday52.Tag())
-	assert.Equal(t, "layout:Mon, 02 Jan 2006 15:04:05 MST;tz:PRC", person.Birthday53.Tag())
-	assert.Equal(t, "layout:02 Jan 06 15:04 MST;tz:PRC", person.Birthday54.Tag())
-	assert.Equal(t, "layout:02 Jan 06 15:04 -0700;tz:PRC", person.Birthday55.Tag())
-	assert.Equal(t, "layout:Monday, 02-Jan-06 15:04:05 MST;tz:PRC", person.Birthday56.Tag())
-	assert.Equal(t, "layout:2006-01-02T15:04:05-07:00;tz:PRC", person.Birthday57.Tag())
-	assert.Equal(t, "layout:2006-01-02T15:04:05.999-07:00;tz:PRC", person.Birthday58.Tag())
-	assert.Equal(t, "layout:2006-01-02T15:04:05.999999-07:00;tz:PRC", person.Birthday59.Tag())
-	assert.Equal(t, "layout:2006-01-02T15:04:05.999999999-07:00;tz:PRC", person.Birthday60.Tag())
 
 	fmt.Printf("Json string parse to person:\n%+v\n", person)
 }
