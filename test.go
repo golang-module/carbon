0de18cb99864ae9cbf1262a56bae08259fb5f2c6
Peleus <245629560@qq.com>
2023-12-27 18:03:44 +0800

    Change receiver type to pointer in `SetTestNow` method

--- test.go
+++ test.go
@@ -2,25 +2,17 @@ package carbon
 
 // SetTestNow sets a test Carbon instance (real or mock) to be returned when a "now" instance is created.
 // 设置当前测试时间
-func SetTestNow(carbon Carbon) Carbon {
-	return NewCarbon().SetTestNow(carbon)
-}
-
-// SetTestNow sets a test Carbon instance (real or mock) to be returned when a "now" instance is created.
-// 设置当前测试时间
-func (c Carbon) SetTestNow(carbon Carbon) Carbon {
+func (c *Carbon) SetTestNow(carbon Carbon) {
 	c.testNow = carbon.TimestampNano()
 	if carbon.loc != nil {
 		c.loc = carbon.loc
 	}
-	return c
 }
 
 // UnSetTestNow clears a test Carbon instance (real or mock) to be returned when a "now" instance is created.
 // 清除当前测试时间
-func (c Carbon) UnSetTestNow() Carbon {
+func (c *Carbon) UnSetTestNow() {
 	c.testNow = 0
-	return c
 }
 
 // IsSetTestNow report whether there is testing time now.
--- test_test.go
+++ test_test.go
@@ -8,38 +8,40 @@ import (
 func TestCarbon_SetTestNow(t *testing.T) {
 	assert := assert.New(t)
 
-	testNow := Parse("2020-08-05")
+	carbon := NewCarbon()
+	carbon.SetTestNow(Parse("2020-08-05"))
 
-	assert.Equal("2020-08-04", SetTestNow(testNow).Yesterday().ToDateString(), "It should be equal to 2020-08-04")
-	assert.Equal("2020-08-05", SetTestNow(testNow).Now().ToDateString(), "It should be equal to 2020-08-05")
-	assert.Equal("2020-08-06", SetTestNow(testNow).Tomorrow().ToDateString(), "It should be equal to 2020-08-06")
-	assert.Equal(31, SetTestNow(testNow).Parse("1989-08-05").Age(), "It should be equal to 31")
-	assert.Equal(int64(1), SetTestNow(testNow).Parse("2020-07-05").DiffInMonths(), "It should be equal to 1")
-	assert.Equal(int64(4), SetTestNow(testNow).Parse("2020-07-05").DiffInWeeks(), "It should be equal to 4")
-	assert.Equal(int64(31), SetTestNow(testNow).Parse("2020-07-05").DiffInDays(), "It should be equal to 31")
-	assert.Equal(int64(-13), SetTestNow(testNow).Parse("2020-08-05 13:14:15").DiffInHours(), "It should be equal to -13")
-	assert.Equal(int64(-794), SetTestNow(testNow).Parse("2020-08-05 13:14:15").DiffInMinutes(), "It should be equal to -794")
-	assert.Equal(int64(-47655), SetTestNow(testNow).Parse("2020-08-05 13:14:15").DiffInSeconds(), "It should be equal to -47655")
-	assert.Equal("-13 hours", SetTestNow(testNow).Parse("2020-08-05 13:14:15").DiffInString(), "It should be -13 hours")
-	assert.Equal("13 hours", SetTestNow(testNow).Parse("2020-08-05 13:14:15").DiffAbsInString(), "It should be -13 hours")
-	assert.Equal("13 hours from now", SetTestNow(testNow).Parse("2020-08-05 13:14:15").DiffForHumans(), "It should be 13 hours from now")
+	assert.Equal("2020-08-04", carbon.Yesterday().ToDateString(), "It should be equal to 2020-08-04")
+	assert.Equal("2020-08-05", carbon.Now().ToDateString(), "It should be equal to 2020-08-05")
+	assert.Equal("2020-08-06", carbon.Tomorrow().ToDateString(), "It should be equal to 2020-08-06")
+	assert.Equal(31, carbon.Parse("1989-08-05").Age(), "It should be equal to 31")
+	assert.Equal(int64(1), carbon.Parse("2020-07-05").DiffInMonths(), "It should be equal to 1")
+	assert.Equal(int64(4), carbon.Parse("2020-07-05").DiffInWeeks(), "It should be equal to 4")
+	assert.Equal(int64(31), carbon.Parse("2020-07-05").DiffInDays(), "It should be equal to 31")
+	assert.Equal(int64(-13), carbon.Parse("2020-08-05 13:14:15").DiffInHours(), "It should be equal to -13")
+	assert.Equal(int64(-794), carbon.Parse("2020-08-05 13:14:15").DiffInMinutes(), "It should be equal to -794")
+	assert.Equal(int64(-47655), carbon.Parse("2020-08-05 13:14:15").DiffInSeconds(), "It should be equal to -47655")
+	assert.Equal("-13 hours", carbon.Parse("2020-08-05 13:14:15").DiffInString(), "It should be -13 hours")
+	assert.Equal("13 hours", carbon.Parse("2020-08-05 13:14:15").DiffAbsInString(), "It should be -13 hours")
+	assert.Equal("13 hours from now", carbon.Parse("2020-08-05 13:14:15").DiffForHumans(), "It should be 13 hours from now")
 }
 
 func TestCarbon_UnSetTestNow(t *testing.T) {
 	assert := assert.New(t)
 
-	datetime := "2020-08-05"
-	testNow := Parse(datetime)
+	carbon := NewCarbon()
+	carbon.SetTestNow(Parse("2020-08-05"))
+	carbon.UnSetTestNow()
 
-	assert.Equal(Now().ToDateString(), SetTestNow(testNow).UnSetTestNow().Now().ToDateString())
+	assert.Equal(Now().ToDateString(), carbon.Now().ToDateString())
 }
 
 func TestCarbon_IsSetTestNow(t *testing.T) {
 	assert := assert.New(t)
 
-	datetime := "2020-08-05"
-	testNow := Parse(datetime)
-
+	carbon := NewCarbon()
 	assert.Equal(false, Now().IsSetTestNow(), "It should be equal to false")
-	assert.Equal(true, SetTestNow(testNow).IsSetTestNow(), "It should be equal to true")
+
+	carbon.SetTestNow(Parse("2020-08-05"))
+	assert.Equal(true, carbon.IsSetTestNow(), "It should be equal to true")
 }
