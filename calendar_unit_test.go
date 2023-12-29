d6916232278c17a20c87f523b0ee2378db9fd881
example <exmaple@example.com>
2023-12-29 15:30:21 +0800

    Add VietName lunar calendar

--- calendar_unit_test.go
+++ calendar_unit_test.go
@@ -24,7 +24,26 @@ func TestError_Lunar(t *testing.T) {
 	assert.NotNil(t, l.Error)
 }
 
+func TestCarbon_VLunar(t *testing.T) {
+	l := Parse("2024-01-18").VLunar()
+	assert.Nil(t, l.Error)
+	assert.Equal(t, "2023-12-08 00:00:00", l.String())
+}
+
+func TestCreateFromVLunar(t *testing.T) {
+	l := CreateFromVLunar(2023, 12, 11, 0, 0, 0, false)
+	assert.Nil(t, l.Error)
+	assert.Equal(t, "2024-01-21 00:00:00", l.String())
+}
+
+func TestError_VLunar(t *testing.T) {
+	l := Parse("xxx").VLunar()
+	assert.NotNil(t, l.Error)
+}
+
 func TestName(t *testing.T) {
 	r := CreateFromLunar(2023, 12, 11, 0, 0, 0, false).ToDateTimeString()
 	fmt.Println(r)
+	r3 := CreateFromVLunar(2023, 12, 11, 0, 0, 0, false).ToDateTimeString()
+	fmt.Println(r3)
 }
