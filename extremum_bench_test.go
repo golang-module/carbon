430c28fc1fda27ba6cc213961449c40c91740eb6
Peleus <245629560@qq.com>
2024-01-02 23:21:50 +0800

    v2.3.2 提高性能测试覆盖率

--- extremum_bench_test.go
+++ extremum_bench_test.go
@@ -7,11 +7,23 @@ func BenchmarkCarbon_Closest(b *testing.B) {
 	for n := 0; n < b.N; n++ {
 		now.Closest(Parse("2020-08-05"), Parse("2022-08-05"))
 	}
+	for n := 0; n < b.N; n++ {
+		now.Closest(Parse("xxx"), Parse("2022-08-05"))
+	}
+	for n := 0; n < b.N; n++ {
+		now.Closest(Parse("2020-08-05"), Parse("xxx"))
+	}
 }
 
 func BenchmarkCarbon_Farthest(b *testing.B) {
 	now := Now()
 	for n := 0; n < b.N; n++ {
 		now.Farthest(Parse("2020-08-05"), Parse("2022-08-05"))
+	}
+	for n := 0; n < b.N; n++ {
+		now.Farthest(Parse("xxx"), Parse("2022-08-05"))
+	}
+	for n := 0; n < b.N; n++ {
+		now.Farthest(Parse("2020-08-05"), Parse("xxx"))
 	}
 }
