01fce0ca039a0ded6c11774bf357e9ba48db8814
Peleus <245629560@qq.com>
2024-01-04 01:08:57 +0800

    Improve benchmark testing coverage

--- extremum_bench_test.go
+++ extremum_bench_test.go
@@ -5,7 +5,10 @@ import "testing"
 func BenchmarkCarbon_Closest(b *testing.B) {
 	now := Now()
 	for n := 0; n < b.N; n++ {
-		now.Closest(Parse("2020-08-05"), Parse("2022-08-05"))
+		now.Closest(Yesterday(), Yesterday().AddDay())
+	}
+	for n := 0; n < b.N; n++ {
+		now.Closest(Yesterday().AddDay(), Yesterday())
 	}
 	for n := 0; n < b.N; n++ {
 		now.Closest(Parse("xxx"), Parse("2022-08-05"))
@@ -18,7 +21,10 @@ func BenchmarkCarbon_Closest(b *testing.B) {
 func BenchmarkCarbon_Farthest(b *testing.B) {
 	now := Now()
 	for n := 0; n < b.N; n++ {
-		now.Farthest(Parse("2020-08-05"), Parse("2022-08-05"))
+		now.Farthest(now.AddYear(), Yesterday())
+	}
+	for n := 0; n < b.N; n++ {
+		now.Farthest(Yesterday(), now.AddYear())
 	}
 	for n := 0; n < b.N; n++ {
 		now.Farthest(Parse("xxx"), Parse("2022-08-05"))
