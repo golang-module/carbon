19e129638dce5d43e1b2a3f35808e3fb43a0b494
Peleus <245629560@qq.com>
2024-01-28 10:25:22 +0800

    ToStdTime->StdTime

--- calendar.go
+++ calendar.go
@@ -12,15 +12,14 @@ func (c Carbon) Lunar() (l lunar.Lunar) {
 		l.Error = c.Error
 		return l
 	}
-	return lunar.FromGregorian(c.ToStdTime()).ToLunar()
+	return lunar.FromGregorian(c.StdTime()).ToLunar()
 }
 
 // CreateFromLunar creates a Carbon instance from Lunar date and time.
 // 从 农历日期 创建 Carbon 实例
 func CreateFromLunar(year, month, day, hour, minute, second int, isLeapMonth bool) Carbon {
-	c := NewCarbon()
-	c.time = lunar.FromLunar(year, month, day, hour, minute, second, isLeapMonth).ToGregorian().Time
-	return c
+	t := lunar.FromLunar(year, month, day, hour, minute, second, isLeapMonth).ToGregorian().Time
+	return CreateFromStdTime(t)
 }
 
 // Julian converts gregorian calendar to Julian calendar
@@ -29,13 +28,12 @@ func (c Carbon) Julian() (j julian.Julian) {
 	if c.Error != nil {
 		return j
 	}
-	return julian.FromGregorian(c.ToStdTime()).ToJulian()
+	return julian.FromGregorian(c.StdTime()).ToJulian()
 }
 
 // CreateFromJulian creates a Carbon instance from Julian Day.or Modified Julian Day
 // 从 儒略日/简化儒略日 创建 Carbon 实例
 func CreateFromJulian(f float64) Carbon {
-	c := NewCarbon()
-	c.time = julian.FromJulian(f).ToGregorian().Time
-	return c
+	t := julian.FromJulian(f).ToGregorian().Time
+	return CreateFromStdTime(t)
 }
