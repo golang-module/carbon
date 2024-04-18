93cc1e6fd715e42f529fbd0511a6dfb1aecbf5a8
Peleus <245629560@qq.com>
2024-04-18 17:43:54 +0800

    Implement `GormDataType` interface

--- database_unit_test.go
+++ database_unit_test.go
@@ -36,5 +36,42 @@ func TestError_Value(t *testing.T) {
 }
 
 func TestCarbon_GormDataType(t *testing.T) {
-	assert.Equal(t, "time", NewCarbon().GormDataType())
+	var carbon Carbon
+	assert.Equal(t, "time", carbon.GormDataType())
+
+	var dateTime DateTime
+	assert.Equal(t, "time", dateTime.GormDataType())
+	var dateTimeMilli DateTimeMilli
+	assert.Equal(t, "time", dateTimeMilli.GormDataType())
+	var dateTimeMicro DateTimeMicro
+	assert.Equal(t, "time", dateTimeMicro.GormDataType())
+	var dateTimeNano DateTimeNano
+	assert.Equal(t, "time", dateTimeNano.GormDataType())
+
+	var date Date
+	assert.Equal(t, "time", date.GormDataType())
+	var dateMilli DateMilli
+	assert.Equal(t, "time", dateMilli.GormDataType())
+	var dateMicro DateMicro
+	assert.Equal(t, "time", dateMicro.GormDataType())
+	var dateNano DateNano
+	assert.Equal(t, "time", dateNano.GormDataType())
+
+	var time Time
+	assert.Equal(t, "time", time.GormDataType())
+	var timeMilli TimeMilli
+	assert.Equal(t, "time", timeMilli.GormDataType())
+	var timeMicro TimeMicro
+	assert.Equal(t, "time", timeMicro.GormDataType())
+	var timeNano TimeNano
+	assert.Equal(t, "time", timeNano.GormDataType())
+
+	var timestamp Timestamp
+	assert.Equal(t, "int", timestamp.GormDataType())
+	var timestampMilli TimestampMilli
+	assert.Equal(t, "int", timestampMilli.GormDataType())
+	var timestampMicro TimestampMicro
+	assert.Equal(t, "int", timestampMicro.GormDataType())
+	var timestampNano TimestampNano
+	assert.Equal(t, "int", timestampNano.GormDataType())
 }
