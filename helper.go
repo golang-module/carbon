d4758223960e51923902995475dc00e47296a26c
gouguoyin <245629560@qq.com>
2024-11-29 11:17:27 +0800

    Optimize `getPositionByTimezone` method

--- helper.go
+++ helper.go
@@ -103,9 +103,6 @@ func format2layout(format string) string {
 // gets a Location instance by a timezone string.
 // 通过时区获取 Location 实例
 func getLocationByTimezone(timezone string) (*time.Location, error) {
-	if timezone == "" {
-		timezone = defaultTimezone
-	}
 	loc, err := time.LoadLocation(timezone)
 	if err != nil {
 		err = invalidTimezoneError(timezone)
